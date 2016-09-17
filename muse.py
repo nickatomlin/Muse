6f396016817da0156672da5059c2a1f7c0c7078b
Greg Carlin <gregcarlin1@gmail.com>
2016-09-17 15:25:47 -0400

    Framework for streaming muse data

--- muse.py
+++ muse.py
@@ -1,10 +1,20 @@
 import subprocess
 
+# takes in a binary string (sequence) and displays the corresponding letter
+def display_letter(sequence):
+    # TODO convert sequence to morse code letter
+    print(sequence)
+
 def main():
     try:
-        print subprocess.check_output(["echo", "hello"])
-    except subprocess.CalledProcessError, e:
-        print "Stdout output:\n", e.output
+        # TODO make sure reference to muse-player works
+        process = subprocess.Popen(["muse-player", "-l", "5000"], stdout=subprocess.PIPE)
+        for line in process.stdout:
+            if "/muse/eeg " in line:
+                # TODO interpret line
+                print(line)
+    except subprocess.CalledProcessError as e:
+        print("Stdout output:\n", e.output)
 
 if __name__ == "__main__":
     main()
